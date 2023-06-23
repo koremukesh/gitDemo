c354208b3c4ffb6049bfe692719bebcc0e3398ac
Mukesh <koremukesh8@gmail.com>
2023-06-23 13:03:53 +0530

    new feature is added in newMultiplyMethod

--- newMultiplyMethod.java
+++ newMultiplyMethod.java
@@ -2,5 +2,7 @@ public class newMultiplyMethod {
     public static void main(String[] args) {
         int a = 10;
         System.out.println("a * b = "+a+10);
+    System.out.println("this is demo");
     }
+    
 }
