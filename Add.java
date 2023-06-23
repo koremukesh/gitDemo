687d1db625a5ea2db7f724ce686af52f4f4ccaec
Mukesh <koremukesh8@gmail.com>
2023-06-23 12:21:26 +0530

    addition program is commited

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+public class Add {
+
+	public static void main(String[] args) {
+       Scanner in=new Scanner(System.in);	
+       
+       System.out.print("Enyer the value of b ");
+       int b=in.nextInt();
+       System.out.print("Enyer the value of c ");
+       int c=in.nextInt();
+      int a=b+c;
+       System.out.println("the addition of two numbers is "+a);
+       in.close();     
+	}
+  
+}
